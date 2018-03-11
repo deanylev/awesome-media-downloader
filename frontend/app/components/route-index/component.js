2fc4c97852b201ee124fc30b53eb3e7eb40cfee7
Dean Levinson <dean.e.levinson@gmail.com>
2018-03-11 12:49:28 +1100

    Handle transcoding errors properly on frontend

--- frontend/app/components/route-index/component.js
+++ frontend/app/components/route-index/component.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 
 const apiHost = config.APP.API_HOST;
 const socket = io(config.APP.SOCKET_HOST);
-const urlRegex =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+const urlRegex = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
 
 export default Component.extend({
   formats: {
@@ -97,6 +97,14 @@ export default Component.extend({
         downloadVideo();
       });
 
+      socket.on('transcoding error', () => {
+        this.setStatus(`Error during conversion. (Video ${videoNumber - 1}/${totalVideos})`, 'danger');
+        this.set('downloadError', true);
+        this.set('inFlight', false);
+        fails++;
+        downloadVideo();
+      });
+
       socket.on('video details', (details) => {
         this.set('downloadError', false);
         this.set('progress', 0);
@@ -121,11 +129,6 @@ export default Component.extend({
               this.set('progress', (response.progress * 100).toFixed(2));
           }
         });
-      });
-
-      socket.on('transcoding error', () => {
-        this.set('downloadError', true);
-        this.set('inFlight', false);
       });
 
       let downloadVideo = () => {
