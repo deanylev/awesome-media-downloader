061aca0b542440d0ef77b6fa3f730ff291e04370
Dean Levinson <dean.e.levinson@gmail.com>
2018-03-13 10:21:30 +1100

    Optimise bash script to build frontend

--- build_frontend.sh
+++ build_frontend.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 (cd frontend; ember build --prod)
-rm views/pages/index.ejs
-rm -r public/assets
-rm -r public/fonts
+rm -r public/assets public/fonts
 mv frontend/dist/index.html views/pages/index.ejs
-mv frontend/dist/assets public/assets
-mv frontend/dist/fonts public/fonts
+mv frontend/dist/assets frontend/dist/fonts public
