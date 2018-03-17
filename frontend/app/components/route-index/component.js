97ed8ee1ad4d40104f9ba82c235f18cb80906c94
Dean Levinson <dean.e.levinson@gmail.com>
2018-03-17 14:40:17 +1100

    Improve URL regex

--- frontend/app/components/route-index/component.js
+++ frontend/app/components/route-index/component.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 
 const apiHost = config.APP.API_HOST;
 const socket = io(config.APP.SOCKET_HOST);
-const urlRegex = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+const urlRegex = /(?:^|\s)((https?:\/\/)?(?:localhost|[\w-]+(?:\.[\w-]+)+)(:\d+)?(\/\S*)?)/;
 
 export default Component.extend({
   urls: '',
