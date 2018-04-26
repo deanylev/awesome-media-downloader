9c6a63108e95034e6d86f7c1a3d4f7685e521b21
Dean Levinson <dean.e.levinson@gmail.com>
2018-04-26 22:14:14 +1000

    Update frontend build script to include favicons

--- build_frontend.sh
+++ build_frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 (cd frontend; ember build --prod)
-rm -r public/assets public/fonts
-mv frontend/dist/index.html views/pages/index.ejs
-mv frontend/dist/assets frontend/dist/fonts public
+rm -r public
+mv frontend/dist public
+mv public/index.html views/pages/index.ejs
